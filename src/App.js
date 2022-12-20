fff0ffd33cdb526fa73265972d83e13ee95f1641
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2022-12-20 21:33:27 +0100

    fixing some errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <p className='text-red-500 text-3xl'>hello world</p>
     </div>
   );
 }
-
-export default App;
