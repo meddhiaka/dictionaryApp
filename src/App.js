167f5af5379e2a447652a559aab3fc87f01addd1
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2022-12-20 21:26:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-red-500 text-3xl'>hello world</p>
+    </div>
+  );
+}
+
+export default App;
