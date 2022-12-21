a9a4203f0b9dae61777f2a9daeaf03ebc2c5dcef
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2022-12-21 01:41:10 +0100

    I/O <=> App.js using useContext Hook

---
+++ src/Components/Header.js
@@ -0,0 +1,58 @@
+import { useState, useContext } from "react"
+import { InputContext } from "../App"
+
+export default function Header() {
+
+    const [value, setValue] = useState("")
+
+    const { inputValue, setInputValue } = useContext(InputContext)
+
+    const handleInput = (e) => {
+        setValue(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        setInputValue(value)
+        setValue("")
+    }
+
+    const handleInputKeyDown = (e) => {
+        if(e.key === "Enter"){
+            setInputValue(value)
+            setValue("")
+        }
+    }
+
+    return (
+        <div className="bg-purple-600 font-poppins">
+            <div className="container mx-auto py-8">
+                <h1 className="text-4xl font-bold text-center text-white font-poppins">A Dictionary</h1>
+                <p className="text-center mt-1 mb-10 text-slate-300 text-lg">Find definitions for words</p>
+                <div className="flex items-center justify-center mt-5 ">
+                    <div className="flex border-4 border-gray-200 rounded">
+                        
+                        <input 
+                            className="outline-none px-4 md:w-80" 
+                            type="text" 
+                            placeholder="Type anything ..."
+                            onChange={handleInput}
+                            value={value}
+                            onKeyDown={handleInputKeyDown}
+                        />
+                        
+                        <button 
+                            className="bg-purple-900 py-2 px-4 text-white"
+                            onClick={handleSubmit}
+                        >
+                            Search
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+            {inputValue && (
+                <h3 className="text-gray-100 text-center py-4">Result for <span className="text-white font-semibold">{inputValue}</span></h3>
+            )}
+        </div>
+    )
+}
