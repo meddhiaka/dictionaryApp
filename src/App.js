99750b9152c154ada4637f2054142aa6f19e0670
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2022-12-21 05:23:32 +0100

    Nesting SubComp components with-in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Components/Header'
 import Result from './Components/Result'
 import { createContext, useState } from 'react'
+import Credits from './Components/Credits'
 
 export const InputContext = createContext()
 
@@ -8,15 +9,17 @@ export const InputContext = createContext()
 export default function App() {
 
   const [inputValue, setInputValue ] = useState("")
+  
   const value = { 
     inputValue, setInputValue
   }
-  console.log(inputValue)
+
   return (
     <InputContext.Provider value={value}>
-      <div className="App">
+      <div className="App font-poppins">
         <Header />
         <Result />
+        <Credits />
       </div>
     </InputContext.Provider>
   )
